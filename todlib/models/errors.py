3c03b1d47f62b928f657244570a1d89052e30e48
PixelCrew Team <pixelcrewteam@gmail.com>
2026-05-15 15:24:38 -0400

    Crear errors.py

---
+++ todlib/models/errors.py
@@ -0,0 +1,26 @@
+class ToDusError(Exception):
+    pass
+
+class AuthenticationError(ToDusError):
+    pass
+
+class TokenExpiredError(ToDusError):
+    pass
+
+class ConnectionLostError(ToDusError):
+    pass
+
+class MessageError(ToDusError):
+    pass
+
+class UploadError(ToDusError):
+    pass
+
+class ParseError(ToDusError):
+    pass
+
+class RateLimitError(ToDusError):
+    pass
+
+class StanzaError(ToDusError):
+    pass
